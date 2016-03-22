bb28ecde3029561a7228e8b94d3c43e731a17e4a
Cyril Vmd <cyril@cyrilwebdesign.com>
2016-03-22 18:10:43 +0100

    Update unit tests

---
+++ test/spec/StormPlayer.js
@@ -0,0 +1,17 @@
+/**
+ * @author Cyril Vermande (cyril@cyrilwebdesign.com)
+ * @license MIT
+ */
+
+// describe("Method Tracklist.prototype.add()", function() {
+	// var tracklist = new Tracklist();
+	// var a = tracklist.add(1);
+	
+	// it("return instance of Tracklist", function(){
+		// expect(a instanceof Tracklist).toBe(true);
+	// });
+
+	// it("add element", function(){
+		// expect(tracklist.elements.length).toBe(1);
+	// });
+// });
