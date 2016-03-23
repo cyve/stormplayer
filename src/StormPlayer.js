d84a09fdaf5d16507934f930f24f76c44ff3433e
Cyril Vmd <cyril@cyrilwebdesign.com>
2016-03-24 00:51:39 +0100

    Update StormPlayer

--- src/StormPlayer.js
+++ src/StormPlayer.js
@@ -3,18 +3,19 @@
  * @license MIT
  */
 
-var StormPlayer = function(params){
-	this.tracklist = new Tracklist();
+var StormPlayer = function(){
+	this.tracklist = new Collection();
 
-	this.repeat = false;
-	this.random = false;
-	this.mute = false;
-	this.duration = 0;
+	this._repeat = false;
+	this._random = false;
+	this._mute = false;
+	this._volume = 1;
+	this._duration = 0;
 
 	//this.playerDefaultEvents = params.events ||
 }
 StormPlayer.prototype.setTracklist = function(tracks, options) {
-	this.tracklist = new Tracklist();
+	this.tracklist = new Collection();
 	var track;
 	for(var i in tracks){
 		track = tracks[i];
@@ -30,3 +31,124 @@ StormPlayer.prototype.setTracklist = function(tracks, options) {
 		//this.tracklist.play(options.current ? 0);
 	}
 };
+
+/**
+ * Get duration
+ * 
+ * @param boolean update Set true to update duration
+ * 
+ * @return integer
+ */
+StormPlayer.prototype.duration = function(update){
+	if(typeof update !== 'undefined' && update){
+		var duration = 0;
+		this.tracklist.forEach(function(element){
+			duration += element.duration();
+		});
+		this._duration = duration;
+	}
+	
+	return this._duration;
+}
+
+/**
+ * Get/set repeat
+ * 
+ * @param boolean value
+ * 
+ * @return mixed
+ * @throw Error if argument is invalid
+ */
+StormPlayer.prototype.repeat = function(value){
+	if(typeof value === 'undefined'){
+		return this._repeat;
+	}
+
+	if(typeof value !== 'boolean'){
+		throw new Error("Invalid argument value (" + value + ")");
+	}
+
+	this._repeat = value;
+
+	return this;
+}
+
+/**
+ * Get/set random
+ * 
+ * @param boolean value
+ * 
+ * @return mixed
+ * @throw Error if argument is invalid
+ */
+StormPlayer.prototype.random = function(value){
+	if(typeof value === 'undefined'){
+		return this._random;
+	}
+
+	if(typeof value !== 'boolean'){
+		throw new Error("Invalid argument value (" + value + ")");
+	}
+
+	this._random = value;
+
+	return this;
+}
+
+/**
+ * Get/set volume
+ * 
+ * @param integer value
+ * 
+ * @return mixed
+ * @throw Error if argument is invalid
+ */
+StormPlayer.prototype.volume = function(value){
+	if(typeof value === 'undefined'){
+		return this._volume;
+	}
+
+	if(typeof value !== 'number' || value < 0 || value > 1){
+		throw new Error("Invalid argument value (" + value + ")");
+	}
+	
+	this.tracklist.forEach(function(element){
+		element.volume(value);
+	});
+
+	this._volume = value;
+
+	return this;
+}
+
+/**
+ * Mute/unmute
+ * 
+ * @param boolean value
+ * 
+ * @return mixed
+ * @throw Error if argument is invalid
+ */
+StormPlayer.prototype.mute = function(value){
+	if(typeof value === 'undefined'){
+		return this._mute;
+	}
+
+	if(typeof value !== 'boolean'){
+		throw new Error("Invalid argument value (" + value + ")");
+	}
+
+	if(value){
+		this.tracklist.forEach(function(element){
+			element.mute();
+		});
+	}
+	else{
+		this.tracklist.forEach(function(element){
+			element.unmute();
+		});
+	}
+	this._mute = value;
+
+	return this;
+}
